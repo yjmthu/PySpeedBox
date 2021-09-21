3863d12b11b9dca7fadb07b12f32020abbbb347e
Wiley2046 <73242138+yjmthu@users.noreply.github.com>
2021-09-22 00:26:49 +0800

    ...

--- dialog.py
+++ dialog.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from PySide6.QtWidgets import QButtonGroup
+from PySide6.QtWidgets import QButtonGroup, QHBoxLayout, QLayout
 from PySide6.QtCore import QFile, QSize, Qt
 from PySide6.QtUiTools import QUiLoader
 
@@ -22,21 +22,24 @@ class Dialog(SpeedWidget):
     def initUi(self):
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.Window | Qt.WindowSystemMenuHint | Qt.WindowMinimizeButtonHint)
 
-        ui_file = QFile(os.fspath(Path(__file__).resolve().parent / "dialog.ui"))
-        ui_file.open(QFile.ReadOnly)
-        self.ui = QUiLoader().load(ui_file, self)
-        ui_file.close()
-
+        self.setLayout(QHBoxLayout())
+        self.layout().setContentsMargins(5,5,5,5)
         qss = open("./qss/dialog_style.qss", 'rb')
         self.setStyleSheet(str(qss.read(), encoding='utf-8'))
         qss.close()
+
+        ui_file = QFile(os.fspath(Path(__file__).resolve().parent / "dialog.ui"))
+        ui_file.open(QFile.ReadOnly)
+        self.ui = QUiLoader().load(ui_file, None)
+        ui_file.close()
+        self.layout().addChildWidget(self.ui)
 
         self.setMinimumSize(QSize(DIALOG_WIDTH, DIALOG_HEIGHT))
         self.setMaximumSize(QSize(DIALOG_WIDTH, DIALOG_HEIGHT))
         #self.ui.lineAppData.setText(self.VarBox.get_dat_path())
         self.move((self.VarBox.ScreenWidth - self.width()) / 2, (self.VarBox.ScreenHeight - self.height()) / 2)
-        self.ui.frame.setStyleSheet("QFrame{background-color:rgba(0, 0, 0, 0);}QLabel{border-radius: 3px;background-color: transparent;}Line{background-color:black};")
-        self.initSpeedBox(self.ui.frame, self.showMinimized, self.close)
+        self.ui.setStyleSheet("QFrame{background-color:rgba(0, 0, 0, 100);}QLabel{border-radius: 3px;background-color: transparent;}Line{background-color:black};")
+        self.initSpeedBox(self.ui, self.showMinimized, self.close)
 
     def setTheme(self):
         self.ui.frame.setStyleSheet("QFrame{background-color:rgba(%1);} QLabel{border-radius: 3px;background-color: transparent;}Line{background-color:black};")
